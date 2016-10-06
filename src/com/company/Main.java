c8d35fdd4347b29b999c9c01d865c2c15f7f0e8a
asingh714 <amsingh714@gmail.com>
2016-10-06 06:06:03 -0400

    advanced functions lab complete

---
+++ src/com/company/Main.java
@@ -0,0 +1,133 @@
+package com.company;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        //#1 - DONE
+//        for (int i = 1; i < 100; i++) {
+//
+//            if(i % 2 == 0) {
+//                System.out.println("True");
+//            } else {
+//                System.out.println("False");
+//            }
+//        }
+
+        //#2 - DONE
+//        String getWord1 = "";
+//        String getWord2 = "";
+//
+//        if (getWord1.equals(getWord2)) {
+//            System.out.println("True");
+//        } else {
+//            System.out.println("False");
+//        }
+
+        //#3 -- Managed to print letters of a word but cannot figure out how to quit at W
+
+//            System.out.println("Please enter a text");
+//            Scanner scan = new Scanner(System.in);
+//
+//
+//            String word = scan.nextLine();
+//
+//            for(char a : word.toCharArray()) {
+//                System.out.println(a);
+//                }
+//
+//            for (char w : word.toCharArray(22)) {
+//                System.out.println(w + "We are quitting here");
+//                }
+
+
+        //#4 - DONE
+//        int randomNumber = (int)(Math.random()*100);
+//        if (randomNumber > 50){
+//            System.out.println(randomNumber);
+//        } else {
+//            System.out.println(-1);
+//        }
+
+//        //#5 -- DONE
+//        Scanner input = new Scanner (System.in);
+//
+//        System.out.println("Please enter a day");
+//        int day = input.nextInt();
+//
+//        System.out.println("Please enter a month(number is fine)");
+//        int month = input.nextInt();
+//
+//        System.out.println("Please enter a year");
+//        int year = input.nextInt();
+//
+//        System.out.println("Day: " + day + " Month: " + month + " Year: " + year);
+//
+//        System.out.println("The next day is ");
+
+
+        //#6 --Could not solve.
+
+        //#7 -- Could not solve.
+
+            //#8 -- DONE!
+//         int num1;
+//         int num2;
+//
+//        if (num1 != num2){
+//            System.out.println((num1 + 1) + (num2 + 1));
+//        } else if (num1 == num2) {
+//            System.out.println((num1 + 2) + (num2 + 2));
+//        }
+
+
+        //#9 -- DONE!
+//        String country = "";
+//
+//        switch (country) {
+//            case "US":
+//            case "Us":
+//            case "us":
+//                System.out.println("United States");
+//                break;
+//            case "DE":
+//            case "de":
+//            case "De":
+//                System.out.println("Germany");
+//                break;
+//            case "HU":
+//            case "Hu":
+//            case "hu":
+//                System.out.println("Hungary");
+//                break;
+//            default:
+//                System.out.println("Unknown");
+//         }
+
+        //#10 -- NOT DONE
+//
+//        Scanner  = new Scanner (System.in);
+//        System.out.println("Please enter a word.");
+//
+//        char n;
+//        switch (n) {
+//            case 'a':
+//            case 'A':
+//            case 'e':
+//            case 'E':
+//            case 'i':
+//            case 'I':
+//            case 'u':
+//            case 'U':
+//            case 'o':
+//            case 'O':
+//                System.out.println(n + " is a vowel");
+//                break;
+//            default:
+//                System.out.println(n + " is a consonant");
+//                break;
+//        }
+
+    }
+
